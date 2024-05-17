6cc30ee0e8d7f84f35c2bce9155fd8831d4d8cae
Pablo Sánchez <ptorres@freepik.com>
2024-05-17 12:39:45 +0200

    feat: better search grep

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -2,29 +2,30 @@ local lsp = require('lsp-zero').preset({})
 local telescope = require('telescope.builtin')
 
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+    local opts = {buffer = bufnr, remap = false}
 
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "<leader>gr", function()
-    telescope.lsp_references()
-  end, opts)
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "<leader>gr", function()
+        telescope.lsp_references()
+    end, opts)
 
-  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  --
-  -- Fuzzy find all the symbols in your current workspace
-  --  Similar to document symbols, except searches over your whole project.
-  vim.keymap.set("n", "<leader>vws", function() telescope.lsp_dynamic_workspace_symbols() end, opts)
-  vim.keymap.set("n", "<leader>ds", function() telescope.lsp_document_symbols() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    --
+    -- Fuzzy find all the symbols in your current workspace
+    --  Similar to document symbols, except searches over your whole project.
+    vim.keymap.set("n", "<leader>vws", function() telescope.lsp_dynamic_workspace_symbols() end, opts)
+    vim.keymap.set("n", "<leader>ds", function() telescope.lsp_document_symbols() end, opts)
 
-  vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
 
-  vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 require'lspconfig'.gopls.setup{}
+
 lsp.setup()
 
 -- You need to setup `cmp` after lsp-zero
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -28,10 +28,6 @@ telescope.setup {
 vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = '[F]ind files' })
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
---this functions requires ripgrep
-vim.keymap.set('n', '<leader>ps', function()
-    builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
 --vim.keymap.set('n', '<leader>ps', builtin.live_grep, { desc = '[S]earch current [W]ord' })
 --
 -- lsp code references
@@ -43,7 +39,14 @@ vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps'
 vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
 vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+
+--vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+--this functions requires ripgrep
+vim.keymap.set('n', '<leader>sg', function()
+    builtin.grep_string({ search = vim.fn.input("May the Force be with you. > ") });
+end)
+
+
 vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
 vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
