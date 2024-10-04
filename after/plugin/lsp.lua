6f8117bd856edd13923834d8a7b00a6b5247d58e
Pablo Sánchez <ptorres@freepik.com>
2024-10-04 09:08:21 +0200

    feat: remove phpactor lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -26,7 +26,6 @@ end)
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 require'lspconfig'.gopls.setup{}
-require'lspconfig'.phpactor.setup{}
 
 lsp.setup()
 
