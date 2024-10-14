58d949853329d502ac5974dccdf3014cc6ec1ac6
Pablo Sánchez <ptorres@freepik.com>
2024-10-14 08:35:49 +0200

    fix: add condition to mypy source

--- after/plugin/nonels.lua
+++ after/plugin/nonels.lua
@@ -1,14 +1,18 @@
 local null_ls = require("null-ls")
 
 null_ls.setup({
-	sources = {
-		null_ls.builtins.formatting.stylua.with({
+    sources = {
+        null_ls.builtins.formatting.stylua.with({
             extra_args = { "--indent-type", "Spaces", "--indent-width", 4 },
         }),
-		null_ls.builtins.completion.spell,
-        null_ls.builtins.diagnostics.mypy,
+        null_ls.builtins.completion.spell,
+        null_ls.builtins.diagnostics.mypy.with({
+            condition = function(utils)
+                return utils.root_has_file({ "mypy.ini" })
+            end,
+        }),
         require("none-ls.formatting.autopep8"),
-	},
+    },
 })
 
 vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { silent = true, desc = "LSP Format" })
