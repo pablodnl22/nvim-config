a2ce88c3d19d685cb348e7dec448b4682f717060
Pablo Sánchez <ptorres@freepik.com>
2024-10-17 13:25:53 +0200

    feat: install trouble.nvim plugin

--- lua/pablodnl22/packer.lua
+++ lua/pablodnl22/packer.lua
@@ -4,76 +4,85 @@
 vim.cmd([[packadd packer.nvim]])
 
 return require("packer").startup(function(use)
-	-- Packer can manage itself
-	use("wbthomason/packer.nvim")
+    -- Packer can manage itself
+    use("wbthomason/packer.nvim")
 
-	use({
-		"nvim-telescope/telescope.nvim",
-		-- or                            , branch = '0.1.x',
-		requires = { { "nvim-lua/plenary.nvim" } },
-	})
+    use({
+        "nvim-telescope/telescope.nvim",
+        -- or                            , branch = '0.1.x',
+        requires = { { "nvim-lua/plenary.nvim" } },
+    })
 
-	use("folke/tokyonight.nvim")
-	-- use {'rose-pine/neovim', as = 'rose-pine'}
-	use("github/copilot.vim")
+    use("folke/tokyonight.nvim")
+    -- use {'rose-pine/neovim', as = 'rose-pine'}
+    use("github/copilot.vim")
 
-	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
-	use("theprimeagen/harpoon")
+    use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
+    use("theprimeagen/harpoon")
 
-	use({
-		"nvim-tree/nvim-tree.lua",
-		requires = {
-			"nvim-tree/nvim-web-devicons", -- optional
-		},
-		config = function()
-			require("nvim-tree").setup({})
-		end,
-	})
+    use({
+        "nvim-tree/nvim-tree.lua",
+        requires = {
+            "nvim-tree/nvim-web-devicons", -- optional
+        },
+        config = function()
+            require("nvim-tree").setup({})
+        end,
+    })
 
-	use("nvim-tree/nvim-web-devicons")
+    use("nvim-tree/nvim-web-devicons")
 
-	use("mbbill/undotree")
-	use("tpope/vim-fugitive")
-	use("lewis6991/gitsigns.nvim")
+    use("mbbill/undotree")
+    use("tpope/vim-fugitive")
+    use("lewis6991/gitsigns.nvim")
 
-	use({
-		"nvimtools/none-ls.nvim",
-		requires = {
+    use({
+        "nvimtools/none-ls.nvim",
+        requires = {
             { "nvim-lua/plenary.nvim" },
             { "nvimtools/none-ls-extras.nvim" },
-		}
-	})
+        },
+    })
 
-	use({
-		"stevearc/aerial.nvim",
-		config = function()
-			require("aerial").setup()
-		end,
-	})
+    use({
+        "stevearc/aerial.nvim",
+        config = function()
+            require("aerial").setup()
+        end,
+    })
 
-	use({
-		"nvim-lualine/lualine.nvim",
-		requires = { "nvim-tree/nvim-web-devicons", opt = true },
-	})
+    use({
+        "folke/trouble.nvim",
+        requires = "kyazdani42/nvim-web-devicons",
+        cmd = { "Trouble" },
+        config = function()
+            require("trouble").setup({})
+        end,
+    })
 
-	use({
-		"VonHeikemen/lsp-zero.nvim",
-		branch = "v2.x",
-		requires = {
-			-- LSP Support
-			{ "neovim/nvim-lspconfig" }, -- Required
-			{ -- Optional
-				"williamboman/mason.nvim",
-				run = function()
-					pcall(vim.cmd, "MasonUpdate")
-				end,
-			},
-			{ "williamboman/mason-lspconfig.nvim" }, -- Optional
+    use({
+        "nvim-lualine/lualine.nvim",
+        requires = { "nvim-tree/nvim-web-devicons", opt = true },
+    })
 
-			-- Autocompletion
-			{ "hrsh7th/nvim-cmp" }, -- Required
-			{ "hrsh7th/cmp-nvim-lsp" }, -- Required
-			{ "L3MON4D3/LuaSnip" }, -- Required
-		},
-	})
+    use({
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v2.x",
+        requires = {
+            -- LSP Support
+            { "neovim/nvim-lspconfig" }, -- Required
+            { -- Optional
+                "williamboman/mason.nvim",
+                run = function()
+                    pcall(vim.cmd, "MasonUpdate")
+                end,
+            },
+            { "williamboman/mason-lspconfig.nvim" }, -- Optional
+
+            -- Autocompletion
+            { "hrsh7th/nvim-cmp" }, -- Required
+            { "hrsh7th/cmp-nvim-lsp" }, -- Required
+            { "L3MON4D3/LuaSnip" }, -- Required
+        },
+    })
 end)
